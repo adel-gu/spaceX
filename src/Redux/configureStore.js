b7192e26807df36d828fb7d6e62e433a1f6cd706
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-16 04:06:25 +0100

    Fix linter errors

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,12 +1,11 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+
 import rocketsReducer from './rockets/rockets';
-import dragonsSlice from './dragons/dragons';
 
-const rootReducer = combineReducers({
-  rockets: rocketsReducer,
-  dragons: dragonsSlice,
+const store = configureStore({
+  reducer: {
+    rockets: rocketsReducer,
+  },
 });
-
-const store = configureStore({ reducer: rootReducer });
 
 export default store;

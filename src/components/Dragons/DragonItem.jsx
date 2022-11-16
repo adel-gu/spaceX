1e7e4a5882e4cf26789ef9efa83559bd48c80a65
adelguit <adeelguitoun@gmail.com>
2022-11-16 14:46:52 +0100

    Dispatch the reserveDragon action

--- src/components/Dragons/DragonItem.jsx
+++ src/components/Dragons/DragonItem.jsx
@@ -1,8 +1,20 @@
+// Hooks
+import { useDispatch } from 'react-redux';
+// props
+import PropTypes from 'prop-types';
+
 // React-bootstrap components
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
 
-const DragonItem = ({name, type, description, img}) => {
+// Redux Actions
+import { reserveDragon } from '../../redux/dragons/dragons';
+
+
+const DragonItem = ({id, name, type, description, reserved, img}) => {
+  const dispatch = useDispatch();
+
   return (
     <li>
       <Card className="flex-md-row border-0 mb-5">
@@ -10,11 +22,21 @@ const DragonItem = ({name, type, description, img}) => {
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Text>{type}</Card.Text>
-          <Card.Text>{description}</Card.Text>
-          <Button variant="primary">Reserve Dragon</Button>
+          <Card.Text>{reserved ? <Badge bg="info" className="me-2">Reserved</Badge> : ""}{description}</Card.Text>
+          <Button variant="primary" onClick={() => dispatch(reserveDragon(id))}>Reserve Dragon</Button>
         </Card.Body>
       </Card>
     </li>
   );
 }
+
+DragonItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
+  img: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
 export default DragonItem;

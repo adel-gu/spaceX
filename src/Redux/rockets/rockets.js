45a463aed02caab400f660ec39b65d89214fdc7a
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-16 14:00:56 +0100

    Remove commented code

--- src/Redux/rockets/rockets.js
+++ src/Redux/rockets/rockets.js
@@ -5,12 +5,6 @@ const FETCH_ROCKETS = 'store/rockets/FETCH_ROCKETS';
 const fetchRockets = createAsyncThunk(FETCH_ROCKETS, async () => {
   const response = await fetch('https://api.spacexdata.com/v3/rockets');
   const rockets = await response.json();
-  /* const rockets = data.map((rocket) => ({
-    id: rocket.id,
-    name: rocket.rocket_name,
-    description: rocket.description,
-    image: rocket.flickr_images[0],
-  })); */
   return rockets;
 });
 

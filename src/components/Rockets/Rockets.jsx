fb6767d5a192a58756f0ab9109e27075a2588e6d
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-16 11:48:06 +0100

    Display rockets

--- src/components/Rockets/Rockets.jsx
+++ src/components/Rockets/Rockets.jsx
@@ -1,13 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Rocket from './Rocket';
+import { fetchRockets } from '../../redux/rockets/rockets';
 
-const Rockets = () => (
-  <main>
-    <ul>
-      <li>
-        <Rocket />
-      </li>
-    </ul>
-  </main>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  const rockets = useSelector((state) => state.rockets);
+
+  return (
+    <main>
+      <ul>
+        {rockets.map((rocket) => (
+          <li key={rocket.id}>
+            <Rocket
+              image={rocket.image}
+              id={rocket.id}
+              description={rocket.description}
+              name={rocket.name}
+            />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
 
 export default Rockets;

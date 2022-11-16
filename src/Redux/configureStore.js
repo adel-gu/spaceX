5e369c24b4f840595228ab6b569671c9508b8c76
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-16 04:18:28 +0100

    Fix missing file extension error for dragons

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import rocketsReducer from 'Redux/rockets/rockets';
+import dragonsSlice from 'Redux/dragons/dragons';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
+    dragons: dragonsSlice,
   },
 });
 

e1e2534f2b27383d10fa62e92c9af7c7c2efe222
Usirusen Ikon <lexicon048@gmail.com>
2022-11-18 09:57:33 +0100

    Update Missions Data in myprofile

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -3,34 +3,54 @@ import { useSelector } from 'react-redux';
 import Stack from 'react-bootstrap/Stack';
 
 function MyProfile() {
+  // Rockets State
   const rockets = useSelector((state) => state.rockets);
-
+  // Missions State
+  const missions = useSelector((state) => state.missions);
+  // Dragons State
   const dragons = useSelector((state) => state.dragons);
+  // Reseved dragons
   const reservedDragons = dragons.filter((dragon) => dragon.reserved === true);
+  // Joined Missions
+  const joinedMission = missions.filter((mission) => mission.reserved === true);
   return (
     <Stack className="container" style={{ border: 'none' }} direction="horizontal" gap={3}>
       {/* Display Rockets */}
-      <div>
-        <h1>My Rockets</h1>
-        <ListGroup style={{ width: '30rem' }}>
-          {
-            rockets.filter((rocket) => rocket.reserved === true).map((rocket) => (
-              <ListGroup.Item key={rocket.id}>{rocket.rocket_name}</ListGroup.Item>
-            ))
-        }
-        </ListGroup>
-      </div>
+      <div className="row">
+        <div className='col-6'>
+          <h1>My Rockets</h1>
+          <ListGroup style={{ width: '30rem' }}>
+            {
+              rockets.filter((rocket) => rocket.reserved === true).map((rocket) => (
+                <ListGroup.Item key={rocket.id}>{rocket.rocket_name}</ListGroup.Item>
+              ))
+          }
+          </ListGroup>
+        </div>
 
-      {/* Display Dragons */}
-      <div>
-        <h1>My Dragons</h1>
-        <ListGroup style={{ width: '30rem' }}>
-          {
-          reservedDragons.map((dragon) => (
-            <ListGroup.Item key={dragon.id}>{dragon.name}</ListGroup.Item>
-          ))
-        }
-        </ListGroup>
+        {/* Display Dragons */}
+        <div className='col-6'>
+          <h1>My Dragons</h1>
+          <ListGroup style={{ width: '30rem' }}>
+            {
+            reservedDragons.map((dragon) => (
+              <ListGroup.Item key={dragon.id}>{dragon.name}</ListGroup.Item>
+            ))
+          }
+          </ListGroup>
+        </div>
+
+        {/* Display Missions */}
+        <div className='col-6'>
+          <h1>My Missions</h1>
+          <ListGroup style={{ width: '30rem' }}>
+            {
+              joinedMission.map((mission) => (
+                <ListGroup.Item key={mission.mission_id}>{mission.mission_name}</ListGroup.Item>
+              ))
+            }
+          </ListGroup>
+        </div>
       </div>
     </Stack>
   );

383e830768d6ce8fbd23d22fe835e38598a1970a
Usirusen Ikon <lexicon048@gmail.com>
2022-11-18 19:37:23 +0100

    Fix linter errors

--- src/components/Dragons/DragonItem.jsx
+++ src/components/Dragons/DragonItem.jsx
@@ -9,34 +9,34 @@ import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 
 // Redux Actions
-import { reserveDragon,  cancelReserveDragon} from '../../redux/dragons/dragons';
+import { reserveDragon, cancelReserveDragon } from '../../redux/dragons/dragons';
 
-
-const DragonItem = ({id, name, type, description, reserved, img}) => {
+const DragonItem = ({
+  id, name, type, description, reserved, img,
+}) => {
   const dispatch = useDispatch();
 
   return (
     <li>
       <Card className="flex-md-row border-0 mb-5">
-        <Card.Img style={{ maxWidth: '18rem', height: "18rem" }} variant="left" src={img} />
+        <Card.Img style={{ maxWidth: '18rem', height: '18rem' }} variant="left" src={img} />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Text>{type}</Card.Text>
           <Card.Text>
-            {(reserved) ? <Badge bg="info" className="me-2">Reserved</Badge> : ""}
+            {(reserved) ? <Badge bg="info" className="me-2">Reserved</Badge> : ''}
             {description}
           </Card.Text>
           {
-          (reserved) ? 
-            <Button variant="btn btn-outline-secondary" onClick={() => dispatch(cancelReserveDragon(id))}>Cancel Reservation</Button>
-            : 
-            <Button variant="primary" onClick={() => dispatch(reserveDragon(id))}>Reserve Dragon</Button>
+          (reserved)
+            ? <Button variant="btn btn-outline-secondary" onClick={() => dispatch(cancelReserveDragon(id))}>Cancel Reservation</Button>
+            : <Button variant="primary" onClick={() => dispatch(reserveDragon(id))}>Reserve Dragon</Button>
           }
         </Card.Body>
       </Card>
     </li>
   );
-}
+};
 
 DragonItem.propTypes = {
   id: PropTypes.string.isRequired,
--- src/components/Missions/Mission.jsx
+++ src/components/Missions/Mission.jsx
@@ -1,10 +1,12 @@
-import Badge from "react-bootstrap/Badge";
-import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
-import { useDispatch } from "react-redux";
-import { joinMission, leavingMission } from "../../redux/Missions/Missions";
+import Badge from 'react-bootstrap/Badge';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import { useDispatch } from 'react-redux';
+import { joinMission, leavingMission } from '../../redux/Missions/Missions';
 
-function Mission({ id, name, description, reserved }) {
+function Mission({
+  id, name, description, reserved,
+}) {
   const dispatch = useDispatch();
 
   const join = () => {
@@ -21,17 +23,17 @@ function Mission({ id, name, description, reserved }) {
         <td>{name}</td>
         <td>{description}</td>
         <td className="border">
-          <Badge bg={reserved ? "info" : "secondary"}>
-            {reserved ? "ACTIVE MEMBER" : "NOT A MEMEBR"}
+          <Badge bg={reserved ? 'info' : 'secondary'}>
+            {reserved ? 'ACTIVE MEMBER' : 'NOT A MEMEBR'}
           </Badge>
         </td>
         <td className="border">
           <Button
             id={id}
             onClick={reserved ? leave : join}
-            variant={reserved ? "outline-danger" : "outline-secondary"}
+            variant={reserved ? 'outline-danger' : 'outline-secondary'}
           >
-            {reserved ? "Leave Misson" : "Join Mission"}
+            {reserved ? 'Leave Misson' : 'Join Mission'}
           </Button>
         </td>
       </tr>
@@ -43,7 +45,7 @@ Mission.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  reserved: PropTypes.string,
+  reserved: PropTypes.string.isRequired,
 };
 
 export default Mission;

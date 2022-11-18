c41c739046612f4ed0b4278502f6b60e2d0112be
Usirusen Ikon <lexicon048@gmail.com>
2022-11-18 11:23:49 +0100

    Write test for missions

---
+++ src/tests/missions.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import Missions from '../components/Missions/Missions';
+import { fetchMissions, joinMission, leavingMission } from '../redux/Missions/Missions';
+
+describe('Missions renders correctly', () => {
+  test('Renders missions Correctly', () => {
+    const missions = render(
+      <Provider store={store}>
+        <Router>
+          <Missions />
+        </Router>
+      </Provider>,
+    );
+    expect(missions).toMatchSnapshot();
+  });
+});
+
+describe('Missions reducers', () => {
+  it('should fetch missions', async () => {
+    await store.dispatch(fetchMissions());
+    expect(store.getState().missions.length).toEqual(10);
+  });
+
+  it('should reserve a missions', () => {
+    store.dispatch(joinMission('9D1B7E0'));
+    expect(store.getState().missions[0].reserved).toBe(true);
+  });
+
+  it('should cancel a reservation', () => {
+    store.dispatch(leavingMission('9D1B7E0'));
+    expect(store.getState().missions[0].reserved).toEqual(false);
+  });
+});
